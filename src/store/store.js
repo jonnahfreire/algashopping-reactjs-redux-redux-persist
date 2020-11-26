5dbe809f31638575d6e4d7fe5efbc5c482acc91f
jonnahfreire <https://github.com/jonnahfreire/>
2020-11-25 23:19:10 -0200

    setting redux and redux-persist

---
+++ src/store/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+import productsReducer from './Products/Products.reducer'
+
+const rootReducer = combineReducers({
+	products: productsReducer
+})
+
+const persistedReducer = persistReducer({
+	key: 'root',
+	storage
+}, rootReducer)
+
+export const store = createStore(persistedReducer)
+
+export const persistedStore = persistStore(store)
